b208f88c2e93e3719e5dfe295fc5792bb4a507a3
Sky1738 <87045314+Peep1738@users.noreply.github.com>
2025-08-16 19:40:55 +0900

    Update fxmanifest.lua

--- reride/fxmanifest.lua
+++ reride/fxmanifest.lua
@@ -4,12 +4,10 @@ game 'gta5'
 author 'sky1738'
 version '2.0'
 
--- クライアントスクリプト（実行順序: config.lua -> main.lua）
 client_scripts {
     'client/main.lua'
 }
 
--- サーバースクリプト（実行順序: config.lua -> server.lua）
 server_scripts {
     'server/server.lua'
 }
